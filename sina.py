ad18786899a778841f087ba213ffd44d551f9a44
futianyi <2509769544@qq.com>
2018-02-28 23:14:17 +0800

    爬取新浪国内新闻

--- sina.py
+++ sina.py
@@ -18,7 +18,6 @@ def get_html(url):
 
 #解析网页得到每个国内新闻链接
 def get_sourceurls(html):
-
 	try:
 		#把json数据转换成python字典,相反的json.dump()可以把数据转换成json数据
 		jd = json.loads(html)
@@ -33,40 +32,39 @@ def get_sourceurls(html):
 
 #得到所需信息
 def get_wantedinfo(newurls, infolist):
-
 	for newurl in newurls:
-			newhtml = get_html(newurl)
-			#把newhtml变成可以使用Xpath的lxml.etree._Element类
-			html = etree.HTML(newhtml)
-			#soup = BeautifulSoup(newhtml, 'lxml')
-			#title = soup.select('.main-title')[0].text
-			try:
-				title = html.xpath('//h1[@class="main-title"]/text()')[0]
-			except:
-				title = 'title error'
-			try:
-				time = html.xpath('//span[@class="date"]/text()')[0]
-			except:
-				time = 'time error'
-				#kw = soup.select('#keywords')[0].get('data-wbkey')
-			try:
-				kw = html.xpath('//div[@class="keywords"]')[0].get('data-wbkey')
-				#article = '+'.join([article.text.strip() for article in soup.select('.article p')])
-				#'+'.join 意思是把后面列表中的每一个元素用"+"连起来
-				article = '+'.join([article.strip() for article in html.xpath('//div[@class="article"]/p/text()')])
-			except:
-				article = 'error'
-			try:
-				ctnum = re.findall('doc-i(.*?).shtml', newurl)
-				cturl = 'http://comment5.news.sina.com.cn/page/info?version=1&format=json&channel=gn&newsid=comos-{}&group=undefined'
-				commenturl = cturl.format(ctnum[0])
-				cthtml = get_html(commenturl)
-				ctdata = json.loads(cthtml)
-				comments = ctdata['result']['count']
-			except:
-				comments = 'error'
-			dic = {'标题': title, '链接': newurl, '参与人数和评论数': comments, '内容': article}
-			infolist.append(dic)
+		newhtml = get_html(newurl)
+		#把newhtml变成可以使用Xpath的lxml.etree._Element类
+		html = etree.HTML(newhtml)
+		#soup = BeautifulSoup(newhtml, 'lxml')
+		#title = soup.select('.main-title')[0].text
+		try:
+			title = html.xpath('//h1[@class="main-title"]/text()')[0]
+		except:
+			title = 'title error'
+		try:
+			time = html.xpath('//span[@class="date"]/text()')[0]
+		except:
+			time = 'time error'
+			#kw = soup.select('#keywords')[0].get('data-wbkey')
+		try:
+			kw = html.xpath('//div[@class="keywords"]')[0].get('data-wbkey')
+			#article = '+'.join([article.text.strip() for article in soup.select('.article p')])
+			#'+'.join 意思是把后面列表中的每一个元素用"+"连起来
+			article = '+'.join([article.strip() for article in html.xpath('//div[@class="article"]/p/text()')])
+		except:
+			article = 'error'
+		try:
+			ctnum = re.findall('doc-i(.*?).shtml', newurl)
+			cturl = 'http://comment5.news.sina.com.cn/page/info?version=1&format=json&channel=gn&newsid=comos-{}&group=undefined'
+			commenturl = cturl.format(ctnum[0])
+			cthtml = get_html(commenturl)
+			ctdata = json.loads(cthtml)
+			comments = ctdata['result']['count']
+		except:
+			comments = 'error'
+		dic = {'标题': title, '链接': newurl, '参与人数和评论数': comments, '内容': article}
+		infolist.append(dic)
 	return infolist
 
 
